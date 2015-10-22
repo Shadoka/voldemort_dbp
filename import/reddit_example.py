6bb2e19c2f8f3e06a19f3ce4273a3e970b0e05f4
Shadoka <tilmann.groeger@gmx.de>
2015-10-22 16:58:44 +0200

    init

---
+++ import/reddit_example.py
@@ -0,0 +1,7 @@
+import praw
+from pprint import pprint
+
+r = praw.Reddit('keyvalue_graphdata by u/theshadoka ver 0.1')
+submissions = r.get_subreddit('leagueoflegends').get_hot(limit=3)
+for x in submissions:
+    print(str(x))
---
+++ import/voldemort_example.py
@@ -0,0 +1,30 @@
+import voldemort
+from pprint import pprint
+
+# verbindungen zu den stores aufbauen
+authorStore = voldemort.StoreClient('authorStore', [{'0', 6666}])
+contentStore = voldemort.StoreClient('contentStore', [{'0', 6666}])
+# author
+authorKey = "shadoka"
+authorValue = ["qwert", "asdf"]
+# vom author verfasster inhalt, also submissions + comments
+contentKey1 = "qwert"
+contentValue1 = {"author":"shadoka", "prev":None, "next":["asdf"]}
+contentKey2 = "asdf"
+contentValue2 = {"author":"shadoka", "prev":"qwert", "next":None}
+# daten in die stores schreiben
+authorStore.put(authorKey, authorValue)
+contentStore.put(contentKey1, contentValue1)
+contentStore.put(contentKey2, contentValue2)
+# lesen der daten
+authorResponse = authorStore.get("shadoka")
+contentsOfShadoka = authorResponse[0][0] # das wirkliche ergebnis steht halt an der stelle
+
+print("contents von shadoka:")
+for content in contentsOfShadoka:
+    x = contentStore.get(str(content))
+    pprint(x[0][0])
+
+authorStore.delete(authorKey)
+contentStore.delete(contentKey1)
+contentStore.delete(contentKey2)
